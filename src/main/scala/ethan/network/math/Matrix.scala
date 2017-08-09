ff2dfe1777b4feaebd46ad86426a9c0c2a47d831
Ethan Hunter <fc.spaceman@gmail.com>
2017-08-08 21:59:24 -0600

    implement more things for Matrix

--- src/main/scala/ethan/network/math/Matrix.scala
+++ src/main/scala/ethan/network/math/Matrix.scala
@@ -1,12 +1,82 @@
 package ethan.network.math
 
-class Matrix(mat: Array[Array[Int]]) {
-  def get(col: Int, row: Int) = mat(col)(row)
+class Matrix(mat: Array[Array[Float]]) {
+  def get(row: Int, col: Int): Float = mat(row)(col)
+
+  def apply(row: Int, col: Int): Float = get(row, col)
+
+  def dim: (Int, Int) = (mat.length, mat(0).length)
+
+  def getRow(i: Int): Array[Float] = {
+    mat(i)
+  }
+
+  def getCol(i: Int): Array[Float] = {
+    val col = for (row <- mat) yield row(i)
+    col
+  }
+
+  def * (that: Matrix): Matrix = {
+    Matrix.mul(this, that)
+  }
+
+  def entryProduct (that: Matrix): Matrix = {
+    Matrix.create(this.dim, (row, col) => this(row, col) * that(row, col))
+  }
+
+  override def hashCode(): Int = {
+    var hash = 7
+    for (r <- mat.indices) {
+      for (c <- mat(0).indices) {
+        hash += r + c
+        hash = hash << 7
+      }
+    }
+    hash
+  }
+
+  override def equals(that: scala.Any): Boolean = {
+    that match {
+      case that: Matrix => equalsMat(that)
+      case _ => false
+    }
+  }
+
+  def equalsMat(that: Matrix): Boolean = {
+    if (dim != that.dim) {
+      return false
+    }
+    var result = true
+    for (r <- mat.indices) {
+      for (c <- mat(0).indices) {
+        result &= this(r, c) == that(r, c)
+      }
+    }
+    result
+  }
 }
 
 object Matrix {
   def identity(rows: Int, cols: Int): Matrix = {
-    val matrix = Array.tabulate(cols, rows)((col, row) => if (row == col) 1 else 0)
-    new Matrix(matrix)
+    create((rows, cols), (row, col) => if (row == col) 1.0F else 0.0F)
+  }
+
+
+  def create(dim: (Int, Int), fill: (Int, Int) => Float) = {
+    val matrix = Array.tabulate(dim._1, dim._2)(fill)
+    new Matrix(mat = matrix)
+  }
+
+  private def mul(left: Matrix, right: Matrix): Matrix = {
+    val dim = (left.dim._1, right.dim._2)
+    create(dim, (r, c) => dot(left.getRow(r), right.getCol(c)))
+  }
+
+  private def dot(left: Array[Float], right: Array[Float]): Float = {
+    var result: Float = 0.0F
+    for (i <- left.indices) {
+      result += left(i) * right(i)
+    }
+    result
   }
 }
--- src/test/scala/ethan/network/math/TestMatrix.scala
+++ src/test/scala/ethan/network/math/TestMatrix.scala
@@ -6,12 +6,13 @@ import org.scalatest.{FlatSpec, Matchers}
 
 
 class TestMatrix extends FlatSpec with Matchers{
-  "A Matrix" should "be built constructed as an identity Matrix" in {
+  "A Matrix" should "be built constructed as an identity Matrix with correct dimension" in {
     val matrix = Matrix.identity(3, 3)
-    matrix.get(0, 0) should be (1)
-    matrix.get(1, 1) should be (1)
-    matrix.get(2, 2) should be (1)
-    matrix.get(0, 2) should be (0)
+    matrix.dim should be (3, 3)
+    matrix(0, 0) should be (1)
+    matrix(1, 1) should be (1)
+    matrix(2, 2) should be (1)
+    matrix(0, 2) should be (0)
   }
 
   it should "throw an IndexOutOfBounds exception if an index is out of bounds" in {
@@ -19,5 +20,42 @@ class TestMatrix extends FlatSpec with Matchers{
     a [IndexOutOfBoundsException] should be thrownBy {
       matrix.get(5, 0)
     }
+  }
+
+  "Multiplying two identity matrices " should "produce an identity matrix" in {
+    val mat1 = Matrix.identity(3, 3)
+    val mat2 = Matrix.identity(3, 3)
+    val res = mat1 * mat2
+
+    res(0, 0) should be (1)
+    res(1, 1) should be (1)
+    res(2, 2) should be (1)
+    res(0, 1) should be (0)
+  }
+
+  "Two identical matrices " should "be equal" in {
+    val mat1 = Matrix.identity(4, 2)
+    val mat2 = Matrix.identity(4, 2)
+
+    mat1.equals(mat2) should be (true)
+    mat2.equals(mat1) should be (true)
+  }
+
+  "Two different matrices" should "be different" in {
+    val matrix1Array: Array[Array[Float]] = Array.tabulate(2, 2)((x, y) => 0.0F)
+    val matrix2Array: Array[Array[Float]] = Array.tabulate(2, 2)((x, y) => 1.0F)
+
+    val mat1 = new Matrix(matrix1Array)
+    val mat2 = new Matrix(matrix2Array)
+
+    mat1.equals(mat2) should be (false)
+    mat2.equals(mat1) should be (false)
+  }
+
+  "Matrices of different sizes" should "not be equal" in {
+    val mat1 = Matrix.identity(3, 2)
+    val mat2 = Matrix.identity(4, 1)
+
+    mat1.equals(mat2) should be (false)
   }
 }
